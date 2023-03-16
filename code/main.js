8fe50e19af00c8336a23311cac3b7c76893a2b7b
Rajnish Kumar <rajnishmsrit@gmail.com>
2023-03-17 00:13:29 +0530

    working

--- code/main.js
+++ code/main.js
@@ -1,9 +1,2 @@
 
-
-console.log(a);
-
-const b = 10;var a = 4;
-
-function add() {
-    console.log(a+b);
-}
+console.log("Hello")
--- server/main.js
+++ server/main.js
@@ -1,17 +1,1 @@
-var a = 5;
-
-{
-    let a=7;
-    console.log(a);
-    {
-        a=100;
-        console.log(a);
-    }
-}
-a = 40;
-console.log(a);
-
-// Scope
-// Overrides
-// let/const overriding var // reverse isn't possible
-// scope as a venn diagram
+console.log("hello world");
